d402b08c056b981877726c69f2748431cb7a7b10
Diogo Dourado <diogo.dourado@quintoandar.com.br>
2024-04-28 15:13:31 -0300

    Add posts page

--- lib/ex_md_blog/page.ex
+++ lib/ex_md_blog/page.ex
@@ -9,18 +9,35 @@ defmodule ExMdBlog.Page do
   @pages_path "/priv/pages/"
   @base_path @pages_path <> "base.html.eex"
   @home_path @pages_path <> "home.html.eex"
+  @posts_path @pages_path <> "posts.html.eex"
 
   @doc "Renders home page and writes HTML file on pages folder"
   @spec render_home() :: path :: String.t()
   def render_home do
     home_full_path = Application.app_dir(:ex_md_blog) <> @home_path
-    home_html = EEx.eval_file(home_full_path, post_callouts: render_post_callouts())
+    home_html = EEx.eval_file(home_full_path)
 
     base_full_path = Application.app_dir(:ex_md_blog) <> @base_path
     full_html = EEx.eval_file(base_full_path, content: home_html)
 
     # removing .eex of the original path
     output_path = String.slice(home_full_path, 0, String.length(home_full_path) - 4)
+
+    :ok = write_file(output_path, full_html)
+    output_path
+  end
+
+  @doc "Renders posts page and writes HTML file on pages folder"
+  @spec render_posts() :: path :: String.t()
+  def render_posts do
+    posts_full_path = Application.app_dir(:ex_md_blog) <> @posts_path
+    posts_html = EEx.eval_file(posts_full_path, post_callouts: render_post_callouts())
+
+    base_full_path = Application.app_dir(:ex_md_blog) <> @base_path
+    full_html = EEx.eval_file(base_full_path, content: posts_html)
+
+    # removing .eex of the original path
+    output_path = String.slice(posts_full_path, 0, String.length(posts_full_path) - 4)
 
     :ok = write_file(output_path, full_html)
     output_path
@@ -33,8 +50,8 @@ defmodule ExMdBlog.Page do
   end
 
   @doc "Renders a list of post pages, writing them down in HTML on pages folder"
-  @spec render_posts() :: list({id :: String.t(), path :: String.t()})
-  def render_posts do
+  @spec render_all_posts() :: list({id :: String.t(), path :: String.t()})
+  def render_all_posts do
     Enum.map(Posts.list(), fn post ->
       post_path = @pages_path <> post.id <> ".html"
       post_full_path = Application.app_dir(:ex_md_blog) <> post_path
